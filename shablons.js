b9ba2cee4b9f955379ad43525962a6845afb735a
rigist <66265205+rigist@users.noreply.github.com>
2021-10-17 19:25:52 +0500

    Add files via upload

---
+++ shablons.js
@@ -0,0 +1,59 @@
+div.closest("#block");  //возвращает ближайший родительский элемент 
+
+
+let name   = document.querySelector('#name');
+
+inputOne.addEventListener('keypress', function(event) {
+		if (event.key == 'Enter'){}
+});
+
+
+let inputAll = document.querySelectorAll('input');
+
+
+let inputAll = document.querySelectorAll('input');
+
+document.querySelector('')
+
+
+for (let i = 0; i < inputAll.length; i++) {
+	inputAll[i]
+}
+
+var attribute = element.getAttribute(attributeName); // возвр. значение атрибута
+
+elem.dataset.productNumber //data-*
+
+elem.classList.add/remove("class")  // доб класс
+
+hasAttribute // проверяет есть ли атрибут
+
+element.setAttribute(name, value);  утановить атрибут
+
+
+///////////
+
+let timerId = setInterval(() => {
+
+	
+
+	divSlider.innerHTML = texts[i];
+	
+	i++;
+	
+	if(i == 3){i =0}
+
+//setInterval
+}, 1000);
+//////////////
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+let field = document.querySelector('#field');
+
+let caption = document.createElement('caption');
+
+caption.innerHTML = ""
